acb763b5c34700cb3bdf76a58c0791f0267a74d2
gavrbot <asakurazik2@gmail.com>
2022-02-08 23:01:21 +0400

    refactored code, added override view functions

--- Swift. Книга 2/Right on target/Right on target/ViewController.swift
+++ Swift. Книга 2/Right on target/Right on target/ViewController.swift
@@ -16,58 +16,83 @@ class ViewController: UIViewController {
     // загаданное число
     var number: Int = 0
     // раунд
-    var round: Int = 0
+    var round: Int = 1
     // сумма очков за раунд
     var points: Int = 0
     
     @IBAction func checkNumber() {
-        // случай, если приложение было только запущено
-        if self.round == 0 {
-            // сгенерировали число
-            self.number = Int.random(in: 1...50)
-            // передаём значение числа в label
-            self.label.text = String(self.number)
-            // инкрементируем раунд
-            self.round += 1
+        // получаем значение числа из слайдера
+        let numSlider = Int(self.slider.value.rounded())
+        // cравниваем число и высчитываем количество очков
+        if numSlider > self.number {
+            self.points += 50 - self.number + numSlider
+        } else if numSlider < self.number {
+            self.points += 50 - numSlider + self.number
         } else {
-            // получаем значение числа из слайдера
-            let numSlider = Int(self.slider.value.rounded())
-            // cравниваем число и высчитываем количество очков
-            if numSlider > self.number {
-                self.points += 50 - self.number + numSlider
-            } else if numSlider < self.number {
-                self.points += 50 - numSlider + self.number
-            } else {
-                self.points += 50
-            }
-            
-            // если конец игры
-            if self.round == 5 {
-                // создаём экземпляр UIAlertController класса
-                let alert = UIAlertController(
-                    title: "Игра окончена",
-                    message: "Вы заработали \(self.points) очков",
-                    preferredStyle: .alert)
-                // добавляем экшн к нему(то есть кнопку)
-                alert.addAction(UIAlertAction(title: "Начать заново", style: .default, handler: nil))
-                self.present(alert, animated: true, completion: nil)
-                self.round = 1
-                self.points = 0
-            // иначе инкрементируем значение раунда
-            } else {
-                self.round += 1
-            }
-            // задаём следующее случайное число
-            self.number = Int.random(in: 1...50)
-            self.label.text = String(self.number)
+            self.points += 50
         }
+        
+        // если конец игры
+        if self.round == 5 {
+            // создаём экземпляр UIAlertController класса
+            let alert = UIAlertController(
+                title: "Игра окончена",
+                message: "Вы заработали \(self.points) очков",
+                preferredStyle: .alert)
+            // добавляем экшн к нему(то есть кнопку)
+            alert.addAction(UIAlertAction(title: "Начать заново", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            self.round = 1
+            self.points = 0
+        // иначе инкрементируем значение раунда
+        } else {
+            self.round += 1
+        }
+        // задаём следующее случайное число
+        self.number = Int.random(in: 1...50)
+        self.label.text = String(self.number)
     }
     
+    // загружает все размещённые на сцене элементы
+    // вызывается только один раз при первой загрузке сцены
+    override func loadView() {
+        super.loadView()
+        print("loadView")
+    }
+    
+    // функция вызывается после завершения загрузки всех элементов(ex: внести правки перед добавление элементов на сцену)
+    // вызывается только один раз при первой загрузке сцены
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("viewDidLoad")
+        // сгенерировали число
+        self.number = Int.random(in: 1...50)
+        // передаём значение числа в label
+        self.label.text = String(self.number)
     }
 
+    // функция вызывается перед тем, как элементы сцены будут добавлены в иерархию графических элементов
+    // вызывается каждый раз, когда сцена добавляется в иерархию
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+    }
+    
+    // функция вызывается после появления элементов на сцену(ex: подходит для запуска анимации или синхронизации данных с сервером)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear")
+    }
 
+    // функции viewWillDisappear и viewDidDisappear вызываются до и после удаления элементов из иерархии view
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear")
+    }
 }
 
