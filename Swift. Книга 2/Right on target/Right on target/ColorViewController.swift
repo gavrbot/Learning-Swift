75cb43a1062ef601d4f6a8d5ecf47793f60fb3fd
gavrbot <asakurazik2@gmail.com>
2022-02-14 23:03:54 +0400

    Created new ColorViewController for second game type, added MainViewController as main, renamed ViewController to NumberViewController

---
+++ Swift. Книга 2/Right on target/Right on target/ColorViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ColorViewController.swift
+//  Right on target
+//
+//  Created by Александр Гаврилов on 13.02.2022.
+//
+
+import UIKit
+
+class ColorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ Swift. Книга 2/Right on target/Right on target/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  Right on target
+//
+//  Created by Александр Гаврилов on 13.02.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+//    lazy var colorViewController = getColorViewController()
+//    lazy var numberViewController = getNumberViewController()
+//
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+//    func getColorViewController() -> ColorViewController {
+//        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+//        let viewCintroller = storyBoard.instantiateViewController(withIdentifier: "ColorViewController")
+//        return viewCintroller as! ColorViewController
+//    }
+//
+//    func getNumberViewController() -> NumberViewController {
+//        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+//        let viewCintroller = storyBoard.instantiateViewController(withIdentifier: "ColorViewController")
+//        return viewCintroller as! NumberViewController
+//    }
+}
