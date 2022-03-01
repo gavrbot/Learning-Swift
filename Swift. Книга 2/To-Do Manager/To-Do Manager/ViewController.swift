eb8034bfd4031efab26346b93c37b51952974819
gavrbot <asakurazik2@gmail.com>
2022-03-01 18:24:33 +0500

    created new project for learning work with UITableViewController

---
+++ Swift. Книга 2/To-Do Manager/To-Do Manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To-Do Manager
+//
+//  Created by Александр Гаврилов on 01.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
