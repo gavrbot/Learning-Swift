e0efac5ee7245bdd8b17dab549b52b0c2c20cca7
gavrbot <asakurazik2@gmail.com>
2022-03-07 23:07:36 +0400

    added functionality of sorting tasks, added functionality to return task to "planned status"

--- Swift. Книга 2/To-Do Manager/To-Do Manager/TaskListController.swift
+++ Swift. Книга 2/To-Do Manager/To-Do Manager/TaskListController.swift
@@ -12,7 +12,19 @@ class TaskListController: UITableViewController {
     // хранилище задач
     var tasksStorage: TasksStorageProtocol = TasksStorage()
     // коллекция задач
-    var tasks: [TaskPriority:[TaskProtocol]] = [:]
+    var tasks: [TaskPriority:[TaskProtocol]] = [:] {
+        didSet {
+            // сортировка списка задач по Int значению в enum TaskStatus
+            // свойство tasksStatusPosition используется вместо enum TaskStatus для уменьшения связность между моделью и контроллером
+            for (tasksGroupPriority, taskGroup) in tasks {
+                tasks[tasksGroupPriority] = taskGroup.sorted { task1, task2 in
+                    let task1position = tasksStatusPosition.firstIndex(of: task1.status)
+                    let task2position = tasksStatusPosition.firstIndex(of: task2.status)
+                    return task1position! < task2position!
+                }
+            }
+        }
+    }
     
     // порядок отображения секций по типам. Индекс в массиве соответствует индексу секции в таблице
     var sectionsTypePositions: [TaskPriority] = [.important, .normal]
@@ -33,16 +45,6 @@ class TaskListController: UITableViewController {
         // загрузка и разбор задач из хранилища
         tasksStorage.loadTasks().forEach{ task in
             tasks[task.type]?.append(task)
-        }
-        
-        // сортировка списка задач по Int значению в enum TaskStatus
-        // свойство tasksStatusPosition используется вместо enum TaskStatus для уменьшения связность между моделью и контроллером
-        for (tasksGroupPriority, taskGroup) in tasks {
-            tasks[tasksGroupPriority] = taskGroup.sorted { task1, task2 in
-                let task1position = tasksStatusPosition.firstIndex(of: task1.status)
-                let task2position = tasksStatusPosition.firstIndex(of: task2.status)
-                return task1position! < task2position!
-            }
         }
     }
     
@@ -74,6 +76,48 @@ class TaskListController: UITableViewController {
         return title
     }
     
+    // функция свайпа для возвращения задачи в статус запланированной
+    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        // получаем данные о текущей задаче, которая нужна для перевода в статус запланированной
+        let taskType = sectionsTypePositions[indexPath.section]
+        guard let _ = tasks[taskType]?[indexPath.row] else {
+            return nil
+        }
+        // проверяем, что задача имеет статус выполненной
+        guard tasks[taskType]![indexPath.row].status == .completed else {
+            return nil
+        }
+        
+        // создаём действие для изменения статуса
+        let actionSwipeInstance = UIContextualAction(style: .normal, title: "Не выполнена") {
+            _, _, _ in
+            self.tasks[taskType]![indexPath.row].status = .planned
+            self.tableView.reloadSections(IndexSet(arrayLiteral: indexPath.section), with: .automatic)
+        }
+        // возвращаем настроенный объект
+        return UISwipeActionsConfiguration(actions: [actionSwipeInstance])
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 1. проверяем существование задачи, соответствующей заданной строке
+        let taskType = sectionsTypePositions[indexPath.section]
+        guard let _  = tasks[taskType]?[indexPath.row] else {
+            return
+        }
+        
+        // 2. убеждаемся, что задача не является выполненной, так как по нажатию выполненные задачи не будут обратно возвращаться в запланированные
+        guard tasks[taskType]![indexPath.row].status == .planned else {
+            // с помощью метода deselectRow происходит снятие выделения нажатой строки
+            tableView.deselectRow(at: indexPath, animated: true)
+            return
+        }
+        
+        // 3. отмечаем задачу как выполненную
+        tasks[taskType]![indexPath.row].status = .completed
+        // 4. перезагружаем секцию таблицы, так как отображение не изменится лишь при изменении данных
+        tableView.reloadSections(IndexSet(arrayLiteral: indexPath.section), with: .automatic)
+    }
+    
     // ячейка для строки таблицы
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // ячейка на основе констреинтов
@@ -84,15 +128,20 @@ class TaskListController: UITableViewController {
     
     // ячейка на основе стека
     private func getConfiguredTaskCell_stack(for indexPath: IndexPath) -> UITableViewCell {
+        // загружаем прототип ячейки по идентификатору
         let cell = tableView.dequeueReusableCell(withIdentifier: "taskCellStack", for: indexPath) as! TaskCell
+        // получаем данные о задаче, которую необходимо вывести в ячейке
         let taskType = sectionsTypePositions[indexPath.section]
         guard let currentTask = tasks[taskType]?[indexPath.row] else {
             return cell
         }
         
+        // изменяем текст в ячейке
         cell.title.text = currentTask.title
+        // изменяем символ в ячейке
         cell.symbol.text = getSymbolForTask(with: currentTask.status)
         
+        // изменяем цвет текста
         if currentTask.status == .planned {
             cell.title.textColor = .black
             cell.symbol.textColor = .black
