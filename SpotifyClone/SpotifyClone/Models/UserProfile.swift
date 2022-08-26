9cff200e46e77c8783262c04f8f733996458c36c
gavrbot <asakurazik2@gmail.com>
2022-08-27 00:40:50 +0400

    updated profile view controller, added necessary pods

--- SpotifyClone/SpotifyClone/Models/UserProfile.swift
+++ SpotifyClone/SpotifyClone/Models/UserProfile.swift
@@ -10,9 +10,8 @@ import Foundation
 struct UserProfile: Codable {
     let country: String
     let display_name: String
-    let explicit_content: [String: Int]
+    let explicit_content: [String: Bool]
     let external_urls: [String: String]
-    // let followers: [String: Codable?]
     let id: String
     let product: String
     let images: [UserImage]
